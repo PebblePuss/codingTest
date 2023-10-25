afbfee305da54c6ed8dbdf3af483571517b40ea2
PebblePuss <stonecat52@naver.com>
2023-10-25 20:53:22 +0900

    coding Test upload

--- codT05.js
+++ codT05.js
@@ -1,3 +1,34 @@
-const solution = ( nums, direc ) => {
-    
+const solution = (nums, direc) => {
+    let func = {
+        "right": (arr) => {
+            let tmp = arr[arr.length - 1];
+
+            for (let i = arr.length - 1; i >= 0; i--) {
+                if (i === 0) {
+                    arr[i] = tmp;
+                } else {
+                    arr[i] = arr[i - 1];
+                }
+            }
+
+            return arr;
+        },
+        "left": (arr) => {
+            let tmp = arr[0];
+
+            for (let i = 0; i < arr.length; i++) {
+                if (i === arr.length - 1) {
+                    arr[i] = tmp;
+                } else {
+                    arr[i] = arr[i + 1];
+                }
+            }
+
+            return arr;
+        }
+    }
+
+    return func[direc](nums);
 }
+
+console.log(solution([1, 2, 3], "right"));
